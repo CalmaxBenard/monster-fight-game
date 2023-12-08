2f358dc9e9f35f698f71e012157738f1d95a5e18
CalmaxBenard <calmaxbenard@gmail.com>
2023-12-08 15:12:51 +0300

    attack logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,22 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currrentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= monsterDamage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currrentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0) {
+    alert("You won!");
+  } else if (currrentPlayerHealth <= 0) {
+    alert("You lost!");
+  }
+}
+
+attackBtn.addEventListener("click", attackHandler);
